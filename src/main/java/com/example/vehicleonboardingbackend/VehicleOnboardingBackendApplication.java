8d5ea60233319b2068b12ef2800e327b4175cadf
Uthpala Bandara <uthpala@fclanka.com>
2025-04-02 13:46:41 +0530

    initial commit

---
+++ src/main/java/com/example/vehicleonboardingbackend/VehicleOnboardingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.vehicleonboardingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleOnboardingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehicleOnboardingBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/vehicleonboardingbackend/VehicleOnboardingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vehicleonboardingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleOnboardingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
